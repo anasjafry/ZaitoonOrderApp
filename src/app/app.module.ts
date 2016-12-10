9d0e032c7060856951d956a29be791230473a54b
Abhijith C S <Abhijith.cs@ad.infosys.com>
2016-12-10 15:18:08 +0530

    Initial Template Restructuring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,19 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
+import { CartPage } from '../pages/cart/cart';
+import { HistoryPage } from '../pages/history/history';
+import { MenuPage } from '../pages/menu/menu';
+import { AccountPage } from '../pages/account/account';
 import { TabsPage } from '../pages/tabs/tabs';
 
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
+    CartPage,
+    HistoryPage,
+    MenuPage,
+    AccountPage,
     TabsPage
   ],
   imports: [
@@ -20,9 +22,10 @@ import { TabsPage } from '../pages/tabs/tabs';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
+    CartPage,
+    HistoryPage,
+    MenuPage,
+    AccountPage,
     TabsPage
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
--- src/pages/cart/cart.ts
+++ src/pages/cart/cart.ts
@@ -3,10 +3,10 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: 'page-cart',
+  templateUrl: 'cart.html'
 })
-export class HomePage {
+export class CartPage {
 
   constructor(public navCtrl: NavController) {
 

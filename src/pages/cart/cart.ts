76a012ec110c7448e18d15c3254b1e659005098d
Anas Jafry <anasrazak@gmail.com>
2016-12-12 00:12:35 +0530

    Cart Page Template

--- src/pages/cart/cart.ts
+++ src/pages/cart/cart.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { AlertController } from 'ionic-angular';
 import { NavController } from 'ionic-angular';
 
 @Component({
@@ -8,8 +8,36 @@ import { NavController } from 'ionic-angular';
 })
 export class CartPage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,
+  public alertCtrl: AlertController
+  ) {
 
+  }
+  doPrompt() {
+    let prompt = this.alertCtrl.create({
+      title: 'Coupon Code',
+      inputs: [
+        {
+          name: 'coupon',
+          placeholder: 'Enter Coupon Code'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Save',
+          handler: data => {
+            console.log('Saved clicked');
+          }
+        }
+      ]
+    });
+    prompt.present();
   }
 
 }

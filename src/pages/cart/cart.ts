028229d25d3178bd25bf8509997f5e1f8aaf1647
Anas Jafry <anasrazak@gmail.com>
2016-12-21 20:11:33 +0530

    Client Side Validations

--- src/pages/cart/cart.ts
+++ src/pages/cart/cart.ts
@@ -5,10 +5,11 @@ import { NavController } from 'ionic-angular';
 var items = [];
 items.push({"itemCode":1001,"itemName":"Paneer Butter Masala","itemQuantity": 4,"itemPrice":90});
 items.push({"itemCode":1002,"itemName":"Channa Butter Masala","itemQuantity": 1,"itemPrice":70});
-items.push({"itemCode":1003,"itemName":"Gobi Masala","itemQuantity": 3,"itemPrice":80});
+items.push({"itemCode":1003,"itemName":"Chicken Masala","itemQuantity": 1,"itemPrice":120});
 localStorage.setItem('myCart', JSON.stringify(items));
 //var info = JSON.parse(localStorage.getItem("itemsInfo"));
 //console.log(info[0]);
+console.log();
 
 
 @Component({
@@ -16,15 +17,15 @@ localStorage.setItem('myCart', JSON.stringify(items));
   templateUrl: 'cart.html'
 })
 export class CartPage {
-  info = JSON.parse(localStorage.getItem("itemsInfo"));
+  info = JSON.parse(localStorage.getItem("myCart"));
   constructor(public navCtrl: NavController,
   public alertCtrl: AlertController
-  ) {  }
-
-
-
+  ) { this.price={
+    text: "1000"
+  } }
 
   doPrompt() {
+    //console.log((JSON.parse(localStorage.getItem("myCart")))[0]);
     let prompt = this.alertCtrl.create({
       title: 'Coupon Code',
       inputs: [
@@ -50,24 +51,63 @@ export class CartPage {
     });
     prompt.present();
   }
-
   
-    
-  // addFunction(code) {
-  // var info = JSON.parse(localStorage.getItem("itemsInfo"));
-  // var i = 0;
-  // var flag = -1;
-  // while(i<info.length) {
-  //   if(info[i].itemCode==code) {
-  //     flag = i;
-  //     break;
-  //   }
-  // i++; 
-  // } 
-  // var item = JSON.parse(localStorage.itemsInfo);  
-  // item[flag].itemQuantity +=1;
-  // localStorage.setItem("itemsInfo", JSON.stringify(item));
-  // console.log((JSON.parse(localStorage.getItem("itemsInfo")))[0]);          
-  // }
+  price() {
+    var info = JSON.parse(localStorage.getItem("myCart"));
+    var i = 0;
+    var sum=0;
+    while(i<info.length) {
+      sum += info[i].itemQuantity*info[i].itemPrice;  
+      i++; 
+    }
+  //this.price=sum;
+  }
+
+  addFunction(code) {
+    var info = JSON.parse(localStorage.getItem("myCart"));
+    var i = 0;
+    var flag = -1;
+    while(i<info.length) {
+      if(info[i].itemCode==code) {
+        flag = i;
+        break;
+      }
+    i++; 
+    } 
+    var item = JSON.parse(localStorage.getItem("myCart"));  
+    item[flag].itemQuantity +=1;
+    localStorage.setItem("myCart", JSON.stringify(item));
+    //document.getElementById("1001").innerHTML  = info.itemQuantity;
+    console.log((JSON.parse(localStorage.getItem("myCart")))[0]);
+  }
+
+  minusFunction(code) {
+    //document.getElementById("item_count_12").innerHTML  = "red";
+    var info = JSON.parse(localStorage.getItem("myCart"));
+    //var x = info[0].itemQuantity +1
+    var i = 0;
+    var flag = -1;
+    while(i<info.length)  {
+      //checks if item aldready in cart and returns the position of that object if exists
+      if(info[i].itemCode==code)  {
+        flag = i;
+        break;
+      }
+      i++; 
+    } 
+    var item = JSON.parse(localStorage.getItem("myCart"));  
+    if(item[flag].itemQuantity > 1) {
+      //console.log(item[flag].itemQuantity);
+      item[flag].itemQuantity -=1;
+    }
+    else  {
+      item.splice(flag, 1);
+    }
+    localStorage.setItem("myCart", JSON.stringify(item));
+    console.log((JSON.parse(localStorage.getItem("myCart")))[0]);
+             
+    //document.getElementById("item_count_12").innerHTML  = "red";
+    //console.log(info.length) ;
+  }
 
 }
